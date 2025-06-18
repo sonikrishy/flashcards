c7f58f9879033e7a37373b49dad136b21c015e59
soniya singhal <ssinghalkrishna@gmail.com>
2025-06-17 18:27:47 -0700

    feat: implement quiz mode feature

---
+++ .windsurf/workflows/experimental-change.md
@@ -0,0 +1,8 @@
+---
+description: experimental change
+---
+
+for each new experiment:
+- create a new Git branch
+- implement changes requested
+- commit and push to GitHub
--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import HomePage from './pages/HomePage';
 import StudyCategoryPage from './pages/StudyCategoryPage';
 import StudyPage from './pages/StudyPage';
 import QuizCategoryPage from './pages/QuizCategoryPage';
+import QuizPage from './pages/QuizPage';
 import StatsPage from './pages/StatsPage';
 import { IncorrectCardsProvider } from './context/IncorrectCardsContext';
 import './App.css';
@@ -14,7 +15,8 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/study" element={<StudyCategoryPage />} />
         <Route path="/study/:category" element={<StudyPage />} />
-        <Route path="/quiz" element={<QuizCategoryPage />} />
+                <Route path="/quiz" element={<QuizCategoryPage />} />
+        <Route path="/quiz/:category" element={<QuizPage />} />
         <Route path="/stats" element={<StatsPage />} />
       </Routes>
     </IncorrectCardsProvider>
